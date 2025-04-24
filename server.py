08cfab379455f790ba1ed441267a12f96fea8e4e
WTL04 <williamluu04@gmail.com>
2025-04-24 16:41:29 -0700

    testing connection to neon DB using psycopg2

--- server.py
+++ server.py
@@ -1,13 +1,60 @@
 import socket
 import os
 from dotenv import load_dotenv
+import psycopg2 
 
 
 def connectDB():
+    # loading in .env variable
+    load_dotenv()
     database_url = os.getenv("DATABASE")
+    print("Database connected!")
+
+def test():
+    load_dotenv()
+    db_url = os.getenv("DATABASE")
+
+    # connect to db using cursor
+    conn = psycopg2.connect(db_url)
+    cursor = conn.cursor()
+
+    # create table
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS playing_with_neon (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            value REAL
+        );
+        """
+    )
+    
+    # insert data
+    cursor.execute("""
+        INSERT INTO playing_with_neon(name, value)
+        SELECT LEFT(md5(i::TEXT), 10), random()
+        FROM generate_series(1, 10) s(i);
+    """)
+
+    # commit changes
+    conn.commit()
+
+    # query and fetch data
+    cursor.execute("SELECT * FROM playing_with_neon;")
+    rows = cursor.fetchall()
+
+    # print results
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    conn.close()
 
 
 def main():
+    # connect to database 
+    connectDB()
+
     # ask the user for IP address and port number
     ip = input("Enter IP address: ")
     port = int(input("Enter port number: "))
@@ -49,4 +96,5 @@ def main():
         TCPSocket.close()
 
 if __name__ == "__main__":
-    main()
+    #main()
+    test()
